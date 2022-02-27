fc9b9aea4f5ffc2cff311692ba21367429aee1ea
CarinWood <carin.wood.85@gmail.com>
2022-02-27 11:29:25 +0100

    update server.js

--- backend/src/controller/TodoController.js
+++ backend/src/controller/TodoController.js
@@ -72,9 +72,74 @@ const updateAgain = (req, res) => {
     res.send(responseFromDB)
 }
 
+const updateEditMode = (id) => {
+    let object =  `Could not find object with id: ${id}`
+        todoArray.forEach(item => {
+            if (id === item.id) {
+                item.editMode = true
+                object = item
+                return
+            }
+        })
+            return todoArray
+  }
+
+const updateEdit = (req, res) => {
+    const id = Number(req.params.id)
+    const responseFromDB = updateEditMode(id)
+    res.status(201).send(responseFromDB)
+}
+
+const deleteTask = (id) => {
+   
+    for (let i = 0; i < todoArray.length; i++) {
+        if (id === todoArray[i].id) {
+            todoArray.splice(i, 1);
+            return todoArray; 
+        }
+     
+ 
+      
+    }
+    return 'error'
+}
+
+
+const deleteTodo = (req, res) => {
+    const id = Number(req.params.id);
+    const responsefromDB = deleteTask(id);
+    res.send(responsefromDB);
+} 
+
+const updateNewTask = (id, newText) => {
+    let object =  `Could not find object with id: ${id}`
+      todoArray.forEach(item => {
+          if (id === item.id) {
+              item.task = newText
+              item.editMode = false
+              object = item
+                return 
+          }
+      })
+   
+     
+     return todoArray
+  }
+
+
+const updateTask = (req, res) => {
+    const id = Number(req.params.id)
+    const newText = req.params.newText
+    const responseFromDB = updateNewTask(id, newText)
+   res.status(201).send(responseFromDB)
+}
+ 
 export default {
     createTodo,
     getTodos,
     updateDone,
-    updateAgain
+    updateAgain,
+    updateEdit,
+    deleteTodo,
+    updateTask
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -29,107 +29,25 @@ app.get('/', AliveController.alive)
 //Get todo array
 app.get('/todo', TodoController.getTodos)
 
+//DELETE TASK
+app.delete("/todo/:id", TodoController.deleteTodo)
 
+//CREATE
+app.post('/todo/add/:task/:name', TodoController.createTodo)
 
+//UPDATE DONE
+app.put('/todo/:id', TodoController.updateDone)
 
-const deleteTask = (id) => {
-   
-    for (let i = 0; i < todoArray.length; i++) {
-        if (id === todoArray[i].id) {
-            todoArray.splice(i, 1);
-            return todoArray; 
-        }
-     
- 
-      
-    }
-    return 'error'
-}
+//UPDATE DONE AGAIN
+app.put('/todo/again/:id', TodoController.updateAgain)
 
+//UPDATE EDIT MODE:
+app.put('/todo/edit/:id', TodoController.updateEdit)
 
-//DELETE
-app.delete("/todo/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const responsefromDB = deleteTask(id);
-    res.send(responsefromDB);
-  })
+//UPDATE TASK
+app.put('/todo/update/:id/:newText', TodoController.updateTask)
 
-  
-
-  //CREATE
-  app.post('/todo/add/:task/:name', TodoController.createTodo)
-
-
-  //UPDATE DONE
-
-  app.put('/todo/:id', TodoController.updateDone)
-
-  //UPDATE DONE AGAIN
- app.put('/todo/again/:id', TodoController.updateAgain)
-
-  //UPDATE EDIT MODE:
-  const updateEditMode = (id) => {
-    let object =  `Could not find object with id: ${id}`
-        todoArray.forEach(item => {
-            if (id === item.id) {
-                item.editMode = true
-                object = item
-                return
-            }
-        })
-            return todoArray
-  }
-  app.put('/todo/edit/:id', (req, res) => {
-      const id = Number(req.params.id)
-      const responseFromDB = updateEditMode(id)
-      res.status(201).send(responseFromDB)
-  })
-
-  //UPDATE TASK
-  const updateNewTask = (id, newText) => {
-    let object =  `Could not find object with id: ${id}`
-      todoArray.forEach(item => {
-          if (id === item.id) {
-              item.task = newText
-              item.editMode = false
-              object = item
-                return 
-          }
-      })
-   
-     
-     return todoArray
-  }
-
-  app.put('/todo/update/:id/:newText', (req, res) => {
-       const id = Number(req.params.id)
-       const newText = req.params.newText
-       const responseFromDB = updateNewTask(id, newText)
-      res.status(201).send(responseFromDB)
-  })
-
-   //Update value
-   const updateValue = (id, newValue) => {
-        let object = `the id is: ${id} and the new value is ${newValue}`
-        todoArray.forEach(item => {
-            if (id === item.id) {
-                item.value = newValue
-                object = item
-                return todoArray
-            }
-        })
-        return todoArray
-}
-
-app.put('/todo/updatevalue/:id/:newValue', (req, res) => {
-    const newValue = req.params.newValue;
-    const id = Number(req.params.id)
-    const responseFromDB = updateValue(id, newValue)
-    res.status(201).send(responseFromDB)
-})
-
-
-  //Get all completed taks
+//Get all completed taks
   const getCompletedTasks = () => {
     const completedTasks = []
         todoArray.forEach(item => {
