1be8670fa51f2573bffca848cb39276509f10745
CarinWood <carin.wood.85@gmail.com>
2022-03-04 18:50:58 +0100

    update

--- frontend/src/components/todoList/TodoList.js
+++ frontend/src/components/todoList/TodoList.js
@@ -14,8 +14,6 @@ import InputArea from '../inputArea/InputArea';
 const TodoList = () => {
 
     const [data, setData] = useState([])
-    const [name, setName] = useState('')
-    const [task, setTask] = useState('')
     const [newTask, setNewTask] = useState('')
  
 
